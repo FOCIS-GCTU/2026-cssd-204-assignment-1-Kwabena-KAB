5cc2fbc2338145c7b3d63f99fae3b2f40afe91ee
Kwabena-KAB <2425402087@live.gctu.edu.gh>
2026-05-31 20:41:19 +0000

    Add files via upload

---
+++ initial.py
@@ -0,0 +1,36 @@
+# File:intials.py
+#Description: Prints out my initials.
+#Assignment Number:2
+#Name:Kwabena Adomako Boadu
+#STUDENT ID:2425402087
+#Email:2425402087@live.gctu.edu.gh
+#Grader:Augustus
+#
+#On my honor,Kwabena Adomako Boadu,this programming assignment is my own work
+# and I have not provide this code to any other student
+
+
+
+
+def main():
+
+    # prints out a blank line
+    print ("")
+    # prints out my initial in capita letter
+    print("...KAB")
+    # prints out a blank line
+    print("")
+
+    print("...KK........KK........AAAAAAAAAAAA........BBBBBBBBBBBB.....")
+    print("...KK.......KK.........AA........AA........BB........BB.....")
+    print("...KK....KK............AA........AA........BB........BB.....")
+    print("...KK...KK.............AA........AA........BB........BB.....")
+    print("...KKK.KK..............AAAAAAAAAAAA........BBBBBBBBB........")
+    print("...KK..KK..............AA........AA........BB.......BB......")
+    print("...KK....KK............AA........AA........BB........BB.....")
+    print("...KK......KK..........AA........AA........BB........BB.....")
+    print("...KK.......KK....**...AA........AA...**...BB........BB...**")
+    print("...KK.........K...**...AA........AA...**...BBBBBBBBBBBB...**")
+    print("")
+
+main()
